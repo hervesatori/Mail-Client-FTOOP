1abf4a1f8a70c1b56fd3fb83879d064a089c88ec
Morpheus852 <Morpheus852@GOTTESWERK.home>
2016-06-07 18:45:47 +0200

    Löschen durch GUI Funktioniert nun ohne Fehler
    Falls Nachricht bereits online gelöscht wurde, wird ein Popup
    aufgerufen, muss noch besser gestalten werden!

--- src/ftoop/mailclient/gui/ButtonListener.java
+++ src/ftoop/mailclient/gui/ButtonListener.java
@@ -1,15 +1,23 @@
 package ftoop.mailclient.gui;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.NoSuchElementException;
 
 import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
+import javax.swing.WindowConstants;
 
 import ftoop.mailclient.daten.Mail;
 
@@ -62,6 +70,41 @@ public class ButtonListener implements ActionListener {
 			} catch (MessagingException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
+			} catch (NoSuchElementException e2){
+				String message = e2.getMessage();
+				String header = "Fehler";
+				JFrame frame = new JFrame();
+				frame.setSize(300,125);
+				frame.setLayout(new GridBagLayout());
+				GridBagConstraints constraints = new GridBagConstraints();
+				constraints.gridx = 0;
+				constraints.gridy = 0;
+				constraints.weightx = 1.0f;
+				constraints.weighty = 1.0f;
+				constraints.insets = new Insets(5, 5, 5, 5);
+				constraints.fill = GridBagConstraints.BOTH;
+				JLabel headingLabel = new JLabel(header);
+				headingLabel.setOpaque(false);
+				frame.add(headingLabel, constraints);
+				constraints.gridx++;
+				constraints.weightx = 0f;
+				constraints.weighty = 0f;
+				constraints.fill = GridBagConstraints.NONE;
+				constraints.anchor = GridBagConstraints.NORTH;
+				JButton cloesButton = new JButton("X");
+				cloesButton.setMargin(new Insets(1, 4, 1, 4));
+				cloesButton.setFocusable(false);
+				frame.add(cloesButton, constraints);
+				constraints.gridx = 0;
+				constraints.gridy++;
+				constraints.weightx = 1.0f;
+				constraints.weighty = 1.0f;
+				constraints.insets = new Insets(5, 5, 5, 5);
+				constraints.fill = GridBagConstraints.BOTH;
+				JLabel messageLabel = new JLabel("<HtMl>"+message);
+				frame.add(messageLabel, constraints);
+				frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+				frame.setVisible(true);
 			}
 	    	  break;
 	       case "Antworten":
